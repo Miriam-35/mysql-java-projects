eb117533672facaf44a1085f2b3c0bc69622c8d6
Miriam-35 <bacon_35@hotmail.com>
2024-03-23 14:48:44 -0700

    week 11 final changes

--- src/main/resources/projects-schema.sql
+++ src/main/resources/projects-schema.sql
@@ -46,3 +46,51 @@ CREATE TABLE material(
   PRIMARY KEY (material_id),
   FOREIGN KEY (project_id) REFERENCES project (project_id) ON DELETE CASCADE
 );
+
+// Windows
+
+INSERT INTO project (project_id, project_name, estimated_hours, actual_hours, difficulty, notes)
+VALUES (2, 'Remove Window', 2, 3, 4, 'Remove window from outside');
+
+INSERT INTO project_category (project_id, category_id)
+VALUES (2, 1);
+
+INSERT INTO step (step_id, project_id, step_text, step_order)
+VALUES (2, 2, 'Remove storm windows', 1);
+
+INSERT INTO step (step_id, project_id, step_text, step_order)
+VALUES (3, 2, 'Score the window edge', 2);
+
+INSERT INTO step (step_id, project_id, step_text, step_order)
+VALUES (4, 2, 'Remove sash weights or springs', 3);
+
+INSERT INTO step (step_id, project_id, step_text, step_order)
+VALUES (5, 2, 'Identify stops', 4);
+
+INSERT INTO step (step_id, project_id, step_text, step_order)
+VALUES (6, 2, 'Score and remove stops', 5);
+
+INSERT INTO step (step_id, project_id, step_text, step_order)
+VALUES (7, 2, 'Fill holes and attach new pieces for stability', 6);
+
+INSERT INTO material (material_id, project_id, material_name, num_required, cost)
+VALUES (2, 2, 'Wood filler', 1, 12.00);
+
+
+// Door Hanger 
+
+INSERT INTO project (project_id, project_name, estimated_hours, actual_hours, difficulty, notes)
+VALUES (1, 'Hang a Door', 4, 3, 3, 'Use the door hangers from Home Depot');
+
+INSERT INTO category (category_id, category_name)
+VALUES (1, 'Doors and Windows');
+
+INSERT INTO project_category (project_id, category_id)
+VALUES (1, 1);
+
+INSERT INTO step (step_id, project_id, step_text, step_order)
+VALUES (1, 1, 'Screw door hangers on the top and bottom of each side of the door frame', 1);
+
+INSERT INTO material (material_id, project_id, material_name, num_required, cost)
+VALUES (1, 1, '2-inch screws', 20, 10.00);
+
